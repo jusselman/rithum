b8f54663d6677729cea544580e00b546c9bc1ac3
jusselman <usselmanjosh@gmail.com>
2022-11-08 20:51:33 -0800

    add token to user route, begin middleware for token

--- config/db.js
+++ config/db.js
@@ -4,8 +4,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-       await mongoose.connect(db);
-       console.log('MongoDB Connected...')
+       await mongoose.connect(db);  
     } catch(err) {
         console.error(err.message);
         process.exit(1);
